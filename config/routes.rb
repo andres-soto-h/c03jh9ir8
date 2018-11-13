eefdb2cbb8aa23ff5b42d578052e30416787dfa2
Andrés Soto <andres.soto.h@outlook.com>
2018-11-12 19:12:22 -0500

    change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :actors
+  resources :actors, except: [:new,:index,:show]
+  get 'actor' => 'actors#index'
+  get 'actor/:id' => 'actors#show'
+  get 'actors/new' => 'actors#new', as: "new_actors"
 end
